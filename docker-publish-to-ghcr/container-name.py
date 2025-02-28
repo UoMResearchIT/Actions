f5c0850a16d24ddc334f2a176ea056e0fc973d10
Donal K. Fellows <donal.k.fellows@manchester.ac.uk>
2025-02-28 11:55:44 +0000

    Tweak

--- docker-publish-to-ghcr/container-name.py
+++ docker-publish-to-ghcr/container-name.py
@@ -4,7 +4,7 @@ container = os.environ.get("CONTAINER", "").lower()
 if container == "":
     # GITHUB_CONTAINER is a two-part name like UoMResearchIT/actions
     container = re.sub(r".*/", "", os.environ["GITHUB_REPOSITORY"]).lower()
-    print(f"determined container name to be {container}")
+    print(f"determined container name to be \033[0;32m{container}\033[0m")
 namespace = os.environ["NAMESPACE"].lower()
 version = os.environ["VERSION"]
 
