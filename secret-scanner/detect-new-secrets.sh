b2b5e6dc5e4e6207c9ac32bce25c2714f87787fb
Donal K. Fellows <donal.k.fellows@manchester.ac.uk>
2026-05-27 10:25:42 +0100

    More adjustments

--- secret-scanner/detect-new-secrets.sh
+++ secret-scanner/detect-new-secrets.sh
@@ -2,40 +2,41 @@
 all_secrets_file=$(mktemp)
 new_secrets_file=$(mktemp)
 command_to_update_baseline_file=$(mktemp)
-if [ -z "$GITHUB_ACTION_PATH" ]; then
-    GITHUB_ACTION_PATH=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
-fi
-GITHUB_STEP_SUMMARY=${GITHUB_STEP_SUMMARY:-'/dev/stderr'}
+: "${GITHUB_ACTION_PATH:=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)}"
+: "${GITHUB_STEP_SUMMARY:=/dev/stderr}"
+: "${PYTHON_PATH:=python}"
 
 fetch_flags_from_file() {
     flag_to_add="$1"
     file_to_check="$2"
 
     flags=()
-    while read line; do
-        if [[ "${line::1}" != '#' ]] && [[ ! -z "$line" ]]; then
-            flag="$flag_to_add $line "
-            flags+="$flag"
-        fi
-    done < "$file_to_check"
+    if [ -r "$file_to_check" ]; then
+        while read line; do
+            if [[ "${line::1}" != '#' ]] && [[ ! -z "$line" ]]; then
+                flag="$flag_to_add $line "
+                flags+="$flag"
+            fi
+        done < "$file_to_check"
+    fi
 
     echo "$flags"
 }
 
 scan_new_secrets() {
-    excluded_files=$(fetch_flags_from_file '--exclude-files' "$EXCLUDE_FILES_PATH" 2>/dev/null)
-    excluded_secrets=$(fetch_flags_from_file '--exclude-secrets' "$EXCLUDE_SECRETS_PATH" 2>/dev/null)
-    excluded_lines=$(fetch_flags_from_file '--exclude-lines' "$EXCLUDE_LINES_PATH" 2>/dev/null)
-    detect_secret_args="$excluded_files $excluded_secrets $excluded_lines $ADDITIONAL_ARGS"
-    echo "Running detect-secrets with args: $detect_secret_args"
+    excluded_files=$(fetch_flags_from_file '--exclude-files' "${EXCLUDE_FILES_PATH}" 2>/dev/null)
+    excluded_secrets=$(fetch_flags_from_file '--exclude-secrets' "${EXCLUDE_SECRETS_PATH}" 2>/dev/null)
+    excluded_lines=$(fetch_flags_from_file '--exclude-lines' "${EXCLUDE_LINES_PATH}" 2>/dev/null)
+    detect_secret_args="${excluded_files} ${excluded_secrets} ${excluded_lines} ${ADDITIONAL_ARGS}"
+    echo "Running detect-secrets with args: ${detect_secret_args}"
 
-    detect-secrets scan $detect_secret_args --baseline "$BASELINE_FILE"
-    detect-secrets audit "$BASELINE_FILE" --report --json > "$all_secrets_file"
-    jq 'map(select(.category == "UNVERIFIED"))' "$all_secrets_file" > "$new_secrets_file"
+    "${PYTHON_PATH}" -m detect-secrets scan ${detect_secret_args} --baseline "${BASELINE_FILE}"
+    "${PYTHON_PATH}" -m detect-secrets audit "${BASELINE_FILE}" --report --json > "${all_secrets_file}"
+    jq 'map(select(.category == "UNVERIFIED"))' "${all_secrets_file}" > "${new_secrets_file}"
 }
 
 advice_if_none_are_secret_short() {
-    jobs_summary_link="$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID/attempts/$GITHUB_RUN_ATTEMPT"
+    jobs_summary_link="${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}/attempts/${GITHUB_RUN_ATTEMPT}"
 
     cat << EOF
 ### If none of these are secrets or you don't care about these secrets
@@ -75,7 +76,7 @@ EOF
 
 markdown_from_new_secrets() {
     secrets_table_body_with_json_chars=$(jq -r '.[] | "|\(.filename)|\(.lines | keys)|\(.types)|"' "$new_secrets_file")
-    secret_table_body=$(echo "$secrets_table_body_with_json_chars" | tr -d '"' | tr -d ']'| tr -d '[')
+    secret_table_body=$(echo "${secrets_table_body_with_json_chars}" | tr -d '["]')
 
     cat << EOF
 # Secret Scanner Report
@@ -93,17 +94,17 @@ For the secrets you have just compromised (it is NOT sufficient to rebase to rem
 EOF
 }
 
-echo "::add-matcher::$GITHUB_ACTION_PATH/secret-problem-matcher.json"
-if [ -z "$BASELINE_FILE" ]; then
+echo "::add-matcher::${GITHUB_ACTION_PATH}/secret-problem-matcher.json"
+if [ -z "${BASELINE_FILE}" ]; then
     export BASELINE_FILE=$(mktemp)
-    NEW_BASELINE=.secrets.baseline
-    jq 'del(.results[])' "$GITHUB_ACTION_PATH/.secrets.baseline" > "$BASELINE_FILE"
+    NEW_BASELINE=.github/secret-scanner/baseline.json
+    jq 'del(.results[])' "${GITHUB_ACTION_PATH}/baseline.json" > "${BASELINE_FILE}"
 else
-    NEW_BASELINE="$BASELINE_FILE"
+    NEW_BASELINE="${BASELINE_FILE}"
 fi
 scan_new_secrets
 
-if [ "$(cat $new_secrets_file)" = "[]" ]; then
+if [ "$(cat "${new_secrets_file}")" = "[]" ]; then
     echo "No new secrets found"
     exit 0
 fi
@@ -112,11 +113,11 @@ markdown_limited_advice=$(markdown_from_new_secrets)
 markdown_console_advice=$(advice_if_none_are_secret_short)
 
 # Print a short message to the console
-echo "$markdown_limited_advice"
-echo "$markdown_console_advice"
+echo "${markdown_limited_advice}"
+echo "${markdown_console_advice}"
 
 # Write a more detailed message to the jobs summary
-echo "$markdown_limited_advice" > "$GITHUB_STEP_SUMMARY"
-advice_if_none_are_secret_verbose >> "$GITHUB_STEP_SUMMARY"
+echo "${markdown_limited_advice}" > "${GITHUB_STEP_SUMMARY}"
+advice_if_none_are_secret_verbose >> "${GITHUB_STEP_SUMMARY}"
 
 exit 1
